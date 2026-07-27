286e3cfc39c0c98b04e77f37c33e5a989c99e005
Martina Pauer <martinapauer38@gmail.com>
2026-07-27 16:07:26 +0000

    Fix: Complete Polygons PNG Image Search With PIL

--- graphics/polygonal.py
+++ graphics/polygonal.py
@@ -7,20 +7,37 @@ class PolygonalImage:
             image.
         '''
         self.polygons: int = 1
-        self.positions: list[tuple[int]] = [(0, 0)]
+        self.positions: list[tuple[int]] = []
         
     def search_polygons(self, absolute_image_path: str):
-        width_px = 0
-        height_px = 0
+        width_px: int = 0
+        height_px: int = 0
+        darkest_color: list[int] = []
+        lighter_color: list[int] = []
+        # Iterates over image pixels color with pillow help
+        from PIL import Image
+        colors = Image.open(absolute_image_path).convert('RGB')
         with open(absolute_image_path, 'rb') as image:
             for height in image.readlines():
                 height_px += 1
-                for width in height.__str__():
+                for width in height.replace('\n', '').__str__():
                     width_px += 1
                     # Search polygon
-                    self.polygons += 1
-                    # Add position
-                    self.positions.append((width_px, height_px))
+                    if ((width_px == 1) and (height_px == 1)):
+                        darkest_color = list(colors.getpixel(width_px, height_px))
+                        lighter_color = darkest_color
+                    else:
+                        state: bool = True
+                        # Compare block to block each color aproximating to the objectives
+                        for color in darkest_color:
+                            state = (state and (int(color) < list(colors.getpixel(width_px, height_px))[darkest_color.index(color)]))
+                        state = True
+                        for color in lighter_color:
+                            state = (state and (int(color) > list(colors.getpixel(width_px, height_px))[lighter_color.index(color)]))    
+                        # Add position When color is Lighter
+                        if state:
+                            self.polygons += 1
+                            self.positions.append((width_px, height_px))
     
     def get_polygons(self) -> list[Polygon]:
         '''
