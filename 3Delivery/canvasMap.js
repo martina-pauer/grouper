744123930903dc97195b2bf88467155c6fefebde
Martina Pauer <martinapauer38@gmail.com>
2026-08-04 14:34:44 +0000

    Fix: Mark Adding

--- 3Delivery/canvasMap.js
+++ 3Delivery/canvasMap.js
@@ -3,10 +3,10 @@ function plotMap(id, longitude, latitude)
     // Make a map using a canvas element
     let canvas = document.getElementById(id);
     // Set correlation width to latitude and height to longitude
-    let width = canvas.width / longitude;
-    let height = canvas.height / latitude;
+    let width = canvas.width / Math.abs(longitude);
+    let height = canvas.height / Math.abs(latitude);
     canvas = canvas.getContext("2d");
     // Draw map From 0 to longitud and from 0 to latitude
     // Add Mark
-    canvas.stroke(width, height, 10, 10)
+    canvas.strokeRect(width, height, 10, 10)
 }
