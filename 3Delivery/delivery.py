b67e004cb06f86bff6e16fd6d591c64727825440
Martina Pauer <martinapauer38@gmail.com>
2026-08-05 18:54:33 +0000

    Add: Python Flask Binary File Making

--- 3Delivery/delivery.py
+++ 3Delivery/delivery.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import Request
+from flask import request
 
 app = Flask(__name__)
 
@@ -21,10 +21,22 @@ def page() -> str:
 def inform() -> str:
     import sqlite3
 
-    form = Request()
     data: list[str] =   [
-                            form.form['model'],
-                            form.form['copies'],
-                            form.form['material']
+                            request.cookies['grouper'],
+                            request.form['copies'],
+                            request.form['material']
                         ]
+    
+    # Load file binary content
+    binary: list[bytes] = []
+
+    for bin in data[0]:
+        if (data.index(bin) % 2 == 0):
+            # Add each Two digits digits As Bytes
+            binary.append(bytes(int((bin + data[data.index(bin)] + 1), 16), 'utf-8'))
+    # Write as binary file in models folder
+    with open(f'models/user_{binary.__hash__()}.3mf', 'wb') as writer:
+        for bin in binary:
+            writer.write(bin)
+                     
     return page()
